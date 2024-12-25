826e40a1135ce2c16609605322528fb62587a865
fadhil1806 <fadhil8637@smk.belajar.id>
2024-12-25 20:38:50 +0700

    try upload file

--- app/api/daftar/anggota/route.ts
+++ app/api/daftar/anggota/route.ts
@@ -1,16 +1,21 @@
 import { NextRequest, NextResponse } from "next/server";
+import { put } from '@vercel/blob';
 
-export async function POST(req: NextRequest) {
+export async function POST(req: NextRequest): Promise<NextResponse> {
   try {
-    const body = await req.json();
-    console.log(body);  // Do something with the data
-    // Process the request here
+    const formData = await req.formData(); // Use formData to parse incoming form data
+    const file = formData.get('picture') as Blob;
 
-    return NextResponse.json({ message: 'An error occurred', status: 500 });
+    if (!file) {
+      return NextResponse.json({ message: 'No file provided' }, { status: 400 });
+    }
 
+    const filename = file.name; // Extract file name
+    const blob = await put(filename, file, { access: 'public' });
+
+    return NextResponse.json({ message: 'File uploaded successfully', blob });
   } catch (error) {
-    console.error('Error reading JSON body:', error);
-    return NextResponse.json({ message: 'An error occurred', status: 500, error });
-
+    console.error('Error uploading file:', error);
+    return NextResponse.json({ message: 'An error occurred', status: 500, error: error.message });
   }
 }
--- app/daftar/page.tsx
+++ app/daftar/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { Button } from "@/components/ui/button";
 import {
   Card,
@@ -15,7 +15,6 @@ import { Textarea } from "@/components/ui/textarea";
 import { toast, ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import axios from "axios";
-
 import {
   Select,
   SelectContent,
@@ -28,6 +27,8 @@ import {
 import { Navbar } from "@/components/navbar";
 
 export default function Page() {
+  const inputFileRef = useRef<HTMLInputElement>(null);
+
   const [formData, setFormData] = useState({
     full_name: "",
     email: "",
@@ -38,20 +39,19 @@ export default function Page() {
     unit_kerja: "",
     asal_sekolah: "",
     harapan: "",
-    picture: null,
+    picture: undefined,
   });
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
     const { id, value } = e.target;
-  
-    // Cek jika input adalah file
+
     if (e.target instanceof HTMLInputElement && e.target.type === "file") {
-      const files = e.target.files; // Dapatkan file dari input
+      const files = e.target.files;
       if (files && files.length > 0) {
-        setFormData((prevData) => ({ ...prevData, [id]: files[0] })); // Simpan file
+        setFormData((prevData) => ({ ...prevData, [id]: files[0] }));
       }
     } else {
-      setFormData((prevData) => ({ ...prevData, [id]: value })); // Simpan teks biasa
+      setFormData((prevData) => ({ ...prevData, [id]: value }));
     }
   };
 
@@ -59,39 +59,29 @@ export default function Page() {
     setFormData((prevData) => ({ ...prevData, [name]: value }));
   };
 
-  const handleSubmit = async (e: React.FormEvent) => {
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-  
-    // Ensure formData is an instance of FormData
+
     const formDataToSend = new FormData();
-    
-    // Append form fields
-    formDataToSend.append('full_name', formData.full_name);
-    formDataToSend.append('email', formData.email);
-    formDataToSend.append('telephone', formData.telephone);
-    formDataToSend.append('jenis_kelamin', formData.jenis_kelamin);
-    formDataToSend.append('tanggal_lahir', formData.tanggal_lahir);
-    formDataToSend.append('jabatan', formData.jabatan);
-    formDataToSend.append('unit_kerja', formData.unit_kerja);
-    formDataToSend.append('asal_sekolah', formData.asal_sekolah);
-    formDataToSend.append('harapan', formData.harapan);
-  
-    // Append file if available
-    const file = formData.picture;
-    console.log(file)
-    // if (file) {
-    //   formDataToSend.append('picture', file);
-    // }
-  
-    try {
-      await axios.post('/api/daftar/anggota', formDataToSend, {
-        headers: {
-          'Content-Type': "application/json"
-        }
+
+    Object.keys(formData).forEach((key) => {
+      const value = formData[key as keyof typeof formData];
+      if (value) {
+        formDataToSend.append(key, value as string | Blob);
       }
-      );
-  
-      // Reset form data after successful submission
+    });
+
+    const file = inputFileRef.current?.files?.[0];
+    if (file) {
+      formDataToSend.append('picture', file);
+    }
+
+    try {
+      await fetch('/api/daftar/anggota', {
+        method: 'POST',
+        body: formDataToSend,
+      });
+
       setFormData({
         full_name: "",
         email: "",
@@ -102,11 +92,11 @@ export default function Page() {
         unit_kerja: "",
         asal_sekolah: "",
         harapan: "",
-        picture: null,
+        picture: undefined,
       });
-  
+
       toast.success("Form submitted successfully!");
-    } catch  {
+    } catch {
       toast.error("An error occurred. Please try again.");
     }
   };
@@ -123,7 +113,6 @@ export default function Page() {
         </CardHeader>
         <CardContent>
           <form onSubmit={handleSubmit} className="grid gap-4">
-            {/* Nama Lengkap */}
             <div className="grid gap-2">
               <Label htmlFor="full_name">Nama Lengkap</Label>
               <Input
@@ -137,19 +126,18 @@ export default function Page() {
               />
             </div>
 
-            {/* Foto */}
             <div className="grid gap-2">
               <Label htmlFor="picture">Foto</Label>
-              <Input
+              <input
+                name="file"
+                multiple={false}
                 id="picture"
                 type="file"
+                ref={inputFileRef}
                 required
-                onChange={handleChange}
-                autoComplete="off"
               />
             </div>
 
-            {/* Email */}
             <div className="grid gap-2">
               <Label htmlFor="email">Email</Label>
               <Input
@@ -160,106 +148,6 @@ export default function Page() {
                 value={formData.email}
                 onChange={handleChange}
                 autoComplete="off"
-              />
-            </div>
-
-            {/* Telephone */}
-            <div className="grid gap-2">
-              <Label htmlFor="telephone">Telephone</Label>
-              <Input
-                id="telephone"
-                type="tel"
-                placeholder="6289544321121"
-                required
-                value={formData.telephone}
-                onChange={handleChange}
-                autoComplete="off"
-              />
-            </div>
-
-            {/* Jenis Kelamin */}
-            <div className="grid gap-2">
-              <Label htmlFor="jenis_kelamin">Jenis Kelamin</Label>
-              <Select onValueChange={(value) => handleSelectChange(value, 'jenis_kelamin')} name="jenis_kelamin">
-                <SelectTrigger>
-                  <SelectValue placeholder="Pilih Kelamin" />
-                </SelectTrigger>
-                <SelectContent>
-                  <SelectGroup>
-                    <SelectLabel>Jenis Kelamin</SelectLabel>
-                    <SelectItem value="Laki-Laki">Laki-Laki</SelectItem>
-                    <SelectItem value="Perempuan">Perempuan</SelectItem>
-                  </SelectGroup>
-                </SelectContent>
-              </Select>
-            </div>
-
-            {/* Tanggal Lahir */}
-            <div className="grid gap-2">
-              <Label htmlFor="tanggal_lahir">Tanggal Lahir</Label>
-              <Input
-                id="tanggal_lahir"
-                type="date"
-                required
-                value={formData.tanggal_lahir}
-                onChange={handleChange}
-              />
-            </div>
-
-            {/* Jabatan */}
-            <div className="grid gap-2">
-              <Label htmlFor="jabatan">Jabatan</Label>
-              <Select onValueChange={(value) => handleSelectChange(value, 'jabatan')} name="jabatan">
-                <SelectTrigger>
-                  <SelectValue placeholder="Pilih Jabatan" />
-                </SelectTrigger>
-                <SelectContent>
-                  <SelectGroup>
-                    <SelectLabel>Jabatan</SelectLabel>
-                    {/* Add select options here */}
-                  </SelectGroup>
-                </SelectContent>
-              </Select>
-            </div>
-
-            {/* Unit Kerja */}
-            <div className="grid gap-2">
-              <Label htmlFor="unit_kerja">Unit Kerja</Label>
-              <Input
-                id="unit_kerja"
-                type="text"
-                placeholder="SMKN 1 KAWALI"
-                required
-                value={formData.unit_kerja}
-                onChange={handleChange}
-                autoComplete="off"
-              />
-            </div>
-
-            {/* Asal Sekolah */}
-            <div className="grid gap-2">
-              <Label htmlFor="asal_sekolah">Asal Sekolah</Label>
-              <Input
-                id="asal_sekolah"
-                type="text"
-                placeholder="SMK TI Bazma"
-                required
-                value={formData.asal_sekolah}
-                onChange={handleChange}
-                autoComplete="off"
-              />
-            </div>
-
-            {/* Harapan */}
-            <div className="grid gap-2">
-              <Label htmlFor="harapan">Harapan Bergabung di Sini</Label>
-              <Textarea
-                id="harapan"
-                name="harapan"
-                placeholder="Tulis pesan disini"
-                required
-                value={formData.harapan}
-                onChange={handleChange}
               />
             </div>
 
